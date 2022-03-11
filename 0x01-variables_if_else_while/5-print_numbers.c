d572ef2dcd559b9b15ade0e6eaacec9f30fb58d0
Pixie19 <felicite.enagnon3@aiesec.net>
2022-03-11 15:15:51 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints 
+ *main - prints all single digit numbers of base
+ *10 starting from 0, followed by a new line 
  * Return: Always 0 (Success)
  */
 int main(void)
