40e4e04916b45a1f6e1a987a05ea9dcba73b1e64
Pixie19 <felicite.enagnon3@aiesec.net>
2022-03-15 10:40:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 *main  prints _putchar, followed by a new line.
+*DESC create a _putchar.c
 *Return: Always 0 (Success)
 */
 int main(void)
@@ -14,5 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return(0);
+return (0);
 }
