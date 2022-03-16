5a0f35d3ebbcd1843fa89da2db5b954eb10a1450
Pixie19 <felicite.enagnon3@aiesec.net>
2022-03-15 19:07:30 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
-void times_table(void) 
+void times_table(void)
 {
 int i, j, k;
 for (i = 0; i < 10; i++)
@@ -20,14 +20,14 @@ if (k < 10 && j != 0)
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar(k + '0');	      
+_putchar(k + '0');
 }
 else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
-_putchar((k % 10) + '0');    
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
