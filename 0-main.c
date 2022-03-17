1bc663b1742863a4f1e9dd457a9326b0aa676ec4
Pixie19 <felicite.enagnon3@aiesec.net>
2022-03-17 09:47:22 -0700

    0-main.c

---
+++ 0-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - entry point
+*Description:it executes, and prints it
+*Return:Always 0 (Success)
+*/
+int main(void) 
+{  
+int n;  
+srand(time(0));  
+n = rand() - RAND_MAX / 2;  
+if (n > 0)printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+return (0); 
+}
